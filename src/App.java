9755f68f46ddaa071ea145222632c9c239b40a7f
Rohindan <rohindan42rohindan@gmail.com>
2026-07-10 23:40:22 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,5 @@
+public class App{
+    public static void main(String[] args){
+        SNAKE snake = new SNAKE();
+    }
+}
---
+++ src/SNAKE.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import java.io.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class SNAKE{
+
+}
